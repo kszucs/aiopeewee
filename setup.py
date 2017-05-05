5169b66aa398bab2f206cbfcbbbbe60f645ab3ff
Szucs Krisztian <szucs.krisztian@gmail.com>
2017-05-05 12:41:05 +0200

    bump version to 0.3.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from os.path import exists
 
 
 setup(name='aiopeewee',
-      version='0.3.2',
+      version='0.3.3',
       packages=['aiopeewee'],
       description='Async Peewee',
       url='http://github.com/kszucs/aiopeewee',
